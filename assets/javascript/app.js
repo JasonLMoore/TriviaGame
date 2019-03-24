acd5f17f0b464633e57250d636220a725ceb32a6
Jason Moore <jason.moore1369@gmail.com>
2019-03-23 20:38:09 -0500

    added new images, created question objects

---
+++ assets/javascript/app.js
@@ -0,0 +1,55 @@
+var questionArr = [];
+
+//answers[0] always = true
+var question1 = {
+    'question': 'In what year did the Kansas City Kings play there first game',
+    'answers': ['1972','1945','1957','1975'],
+}
+    
+var question2 = {
+    'question': 'The Kings franchise has never called which city "home"?',
+    'answers': ['Buffalo','Rochester','Cincinatti','Sacramento'],
+}
+    
+var question3 = {
+    'question': 'What other nickname have the Kings organization opperated under?',
+    'answers': ['Royals','Monarchs','Jazz','Blues'],
+}
+    
+var question4 = {
+    'question': 'Which former Kings player has not been inducted into the NBA Hall of Fame?',
+    'answers': ['Larry Drew','Oscar Robertson','Jo-Jo White','Nate Archibald'],
+    //Bonus Trivia: Oscar Robertson never wore a Kansas City jersey but was a member of the team when they were the Cincinatti Royals
+}
+    
+var question5 = {
+    'question': 'Which former Kansas Jayhawk has never played for the Kings?',
+    'answers': ['Paul Pierce','Jo-Jo White','Frank Mason III','Ben McLemore'],
+}
+    
+var question6 = {
+    'question': "From 1972-1985, which arena was never used for a King's home game?",
+    'answers': ['Sprint Center','Municipal Auditorium ','Omaha Civic Auditorium','Kemper Arena'],
+}
+    
+var question7 = {
+    'question': 'How many playoff apperances did the Kings have while they were in Kansas City?',
+    'answers': ['5','3','7','10'],
+}
+    
+var question8 = {
+    'question': 'Which Kings play won "Rookie of the Year"?',
+    'answers': ['Phil Ford','Nate Archibald','Sam Lacey','Reggie King'],
+}
+    
+var question9 = {
+    'question': 'The Kings won the most games which season?',
+    'answers': ['78-79','73-74','79-80','81-82'],
+}
+    
+var question10 = {
+    'question': 'What year did the Kansas City-Omaha Kings become the Kansas City Kings?',
+    'answers': ['1975','1972','1980','1984'],
+}
+    
+
